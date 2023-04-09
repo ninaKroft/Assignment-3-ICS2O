379ac0d7c65ae707421b7d4f2b0f72bfb3e7dbfa
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-04-09 21:35:57 +0000

    counting toppings

--- calculations.php
+++ calculations.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>SH Pizza</title>
+</head>
+<body>
+<?php
+  $topping = $_POST['formTopping'];
+    $N = count($aDoor);
 
+    echo("You selected $N topping(s): ");
+    for($i=0; $i < $N; $i++)
+    {
+      echo($topping[$i] . " ");
+    }
+?>
+</body>
+</html>
