86795aab513512877de3430db24847b5c29a3531
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-04-11 14:35:48 +0000

    check for 0 toppings

--- calculations.php
+++ calculations.php
@@ -7,17 +7,22 @@
 <body>
 <?php
   $size = $_POST['size'];
-   echo($size);
+   echo("You selected a $size pizza.");
    echo("</br>");
 
   $topping = $_POST['formTopping'];
     $N = count($topping);
 
+    if ($N > 0){
     echo("You selected $N topping(s): ");
     for($i=0; $i < $N; $i++)
     {
       echo($topping[$i] . " ");
     }
+  }
+  else {
+    echo("You did not select any toppings.");
+  }
 ?>
 </body>
 </html>
