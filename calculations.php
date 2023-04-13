cae68cc37ef9ce8e13ac93adcf2127ccd18bb246
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-04-13 14:28:35 +0000

    calculations

--- calculations.php
+++ calculations.php
@@ -46,13 +46,13 @@
 </div>
 
 <?php
+
   $size = $_POST['size'];
    echo("You selected a $size pizza.");
    echo("</br>");
 
   $topping = $_POST['formTopping'];
     $N = count($topping);
-
     if ($N > 0){
     echo("You selected $N topping(s): ");
     for($i=0; $i < $N; $i++)
@@ -63,6 +63,31 @@
   else {
     echo("You did not select any toppings.");
   }
+
+  if ($N == 1){
+    $tPrice = floatval(1.00);
+  }
+  elseif ($N == 2){
+    $tPrice = floatval(1.75);
+  }
+  elseif ($N == 3){
+    $tPrice = floatval(2.50);
+  }
+  elseif ($N == 4){
+    $tPrice = floatval(3.35);
+  }
+
+  if ($size == "Large") {
+    $sPrice = floatval(6.00);
+  }
+  elseif ($size == "Extra large") {
+    $sPrice = floatval(10.00);
+  }
+
+$fPrice =  floatval($sPrice + $tPrice * 1.13);
+
+echo("Your pizza will cost $fPrice $");
+
 ?>
 </body>
 </html>
